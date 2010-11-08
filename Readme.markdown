13c5570250c25273996cb211d60bd9c8672f33bb
Sam Soffes <sam@samsoff.es>
2010-11-07 19:20:22 -0600

    Initial commit

---
+++ Readme.markdown
@@ -0,0 +1,5 @@
+## SSTableViewCell
+
+Simple, fast table view cell based on ABTableViewCell.
+
+Just override `drawContentView:` and write your `drawRect:` code for the cell's inner view.
---
+++ SSTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  SSTableViewCell.h
+//  SSTableViewCell
+//
+//  Created by Sam Soffes on 8/16/10.
+//  Copyright 2010 Sam Soffes. All rights reserved.
+//
+//  Based on ABTableViewCell by Loren Brichter
+//
+
+@interface SSTableViewCell : UITableViewCell {
+	
+@protected
+
+	UIView *_cellView;
+}
+
+- (void)drawContentView:(CGRect)rect;
+
+@end
